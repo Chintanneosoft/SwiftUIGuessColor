31ee89ebbb86188a7519502b5935f867cd3ac393
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-19 12:13:06 +0530

    Initial Commit

---
+++ SwiftUIGuessColor/SwiftUIGuessColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuessColorApp.swift
+//  SwiftUIGuessColor
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuessColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
